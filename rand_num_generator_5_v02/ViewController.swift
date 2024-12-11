dbd583af4f4ebd7987bf1dd47a9b4c85e2ddca74
Tommy H. Yeargin, Jr <88904457+yeargin2021@users.noreply.github.com>
2024-12-11 16:50:01 -0500

    commit

--- rand_num_generator_5_v02/ViewController.swift
+++ rand_num_generator_5_v02/ViewController.swift
@@ -1,19 +1,35 @@
-//
-//  ViewController.swift
-//  rand_num_generator_5_v02
-//
-//  Created by TOMMY YEARGIN on 12/11/24.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Call the function to generate and display a random number
+        let randomNumber = getRandomNumber()
+        print("Random number: \(randomNumber)")
+        displayRandomNumber(randomNumber)
     }
 
+    // Function to generate a random number between 1 and 5
+    func getRandomNumber() -> Int {
+        return Int.random(in: 1...5)
+    }
 
+    // Function to display the random number on the screen
+    func displayRandomNumber(_ number: Int) {
+        let label = UILabel()
+        label.text = "Random Number: \(number)"
+        label.font = UIFont.systemFont(ofSize: 24)
+        label.textColor = .black
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(label)
+        
+        // Center the label in the view
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
 }
-
